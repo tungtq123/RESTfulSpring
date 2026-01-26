9fbf5b8e1621ee4d32c3d9d58e13a11d7664c6c0
tungtq123 <he182488toquoctung@gmail.com>
2026-01-26 17:01:59 +0700

    first commit

---
+++ src/main/java/com/tqt/restfulspring/ResTfulSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tqt.restfulspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tqt/restfulspring/ResTfulSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqt.restfulspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
